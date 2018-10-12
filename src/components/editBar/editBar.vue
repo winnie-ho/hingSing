f5b9fee260a8bdc5dd8afba0f2f6d6e9f7c5c10f
Winnie Ho <winnie-ho@hotmail.co.uk>
2018-10-12 18:22:22 +0100

    Creating a editBar component.

---
+++ src/components/editBar/editBar.vue
@@ -0,0 +1,21 @@
+<template src="./editBar.html"></template>
+<style scoped src='./editBar.css'></style>
+
+<script src>
+  export default {
+    name: 'editBar',
+    data () {
+      return {
+        
+      }
+    },
+    props: {
+
+    },
+    methods: {
+    },
+    computed: {
+    }
+  }
+</script>
+
