1d52195cec8c34c8f02d8fc5b64ad5976abfed77
Winnie Ho <winnie-ho@hotmail.co.uk>
2018-10-12 13:38:03 +0100

    Adding a site footer for shop address.

---
+++ src/components/siteFooter/siteFooter.vue
@@ -0,0 +1,21 @@
+<template src="./siteFooter.html"></template>
+<style scoped src='./siteFooter.css'></style>
+
+<script src>
+  export default {
+    name: 'siteFooter',
+    data () {
+      return {
+        
+      }
+    },
+    props: {
+
+    },
+    methods: {
+    },
+    computed: {
+    }
+  }
+</script>
+
