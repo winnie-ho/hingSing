0795e16189a26b3053e5bcbb6a1813aa0059acca
Winnie Ho <winnie-ho@hotmail.co.uk>
2018-10-17 18:24:05 +0100

    Registering user and showing user feedback on success or error.

--- src/components/registerForm/registerForm.vue
+++ src/components/registerForm/registerForm.vue
@@ -11,20 +11,33 @@
           emailConfirm: '',
           password: '',
           name: ''
+        },
+        registerResult: {
+          success: null
         }
       }
     },
     props: [ ],
     methods: {
       registerUser(){
-        console.log('REGISTER USER PROCESS');
+        return firebase
+        .auth()
+        .createUserWithEmailAndPassword(this.form.email, this.form.password)
+        .then((value) => {
+          this.registerResult = { success: true, value };
+        })
+        .catch((error) => {
+          this.registerResult = { success: false, error };
+        })
       },
       showLoginForm(){
         this.$emit('onSetRegisteredUser', true);
       }
     },
     computed: {
-
+      errorMessage(){
+        return this.registerResult.error ? this.registerResult.error.message : '';
+      }
     }
   }
 </script>
