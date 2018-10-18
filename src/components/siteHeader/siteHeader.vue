0c8fea813d5005b38877c4aa34224ba95f4b02d2
Winnie Ho <winnie-ho@hotmail.co.uk>
2018-10-18 21:32:31 +0100

    Sign out button.

--- src/components/siteHeader/siteHeader.vue
+++ src/components/siteHeader/siteHeader.vue
@@ -6,15 +6,30 @@
     name: 'siteHeader',
     data () {
       return {
-        
+        signOutResult: null
       }
     },
     props: {
 
     },
     methods: {
+      signOutUser(){
+        return firebase
+        .auth()
+        .signOut()
+        .then((value) => {
+          this.signOutResult = { success: true, value };
+          this.$store.dispatch('setUser', value )
+        })
+        .catch((error) => {
+          this.signOutResult = { success: false, error };
+        });
+      }
     },
     computed: {
+      user(){
+        return this.$store.state.user;
+      }
     }
   }
 </script>
