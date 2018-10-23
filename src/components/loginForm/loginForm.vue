31d544643c6cdc7a29580f2fc47a929497de06ea
Winnie Ho <winnie-ho@hotmail.co.uk>
2018-10-23 14:52:23 +0100

    Setting the client upon user login.

--- src/components/loginForm/loginForm.vue
+++ src/components/loginForm/loginForm.vue
@@ -16,6 +16,9 @@
       }
     },
     props: [ ],
+    mounted(){
+      this.$store.dispatch('fetchClients');
+    },
     methods: {
       loginUser(){
         return firebase
@@ -23,7 +26,8 @@
         .signInWithEmailAndPassword(this.form.email, this.form.password)
         .then((value) => {
           this.loginResult = { success: true, value };
-          this.$store.dispatch('setUser', value )
+          this.$store.dispatch('setUser', value );
+          this.setUserClient();
         })
         .catch((error) => {
           this.loginResult = { success: false, error };
@@ -31,11 +35,18 @@
       },
       showRegisterForm(){
         this.$emit('onSetRegisteredUser', false)
+      },
+      setUserClient(){
+        const userClient = this.clients.find(client => client.email === this.form.email);
+        this.$store.dispatch('setClient', userClient);
       }
     },
     computed: {
       errorMessage(){
         return this.loginResult.error ? this.loginResult.error.message : '';
+      },
+      clients(){
+        return this.$store.state.clients;
       }
     }
   }
