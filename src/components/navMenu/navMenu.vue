010b2ff5005371981655ce061cc11571fd2b60ba
Winnie Ho <winnie-ho@hotmail.co.uk>
2018-10-30 17:16:36 +0000

    Putting in a switch statement for the pages.

--- src/components/navMenu/navMenu.vue
+++ src/components/navMenu/navMenu.vue
@@ -2,32 +2,49 @@
 <style scoped src='./navMenu.css'></style>
 
 <script src>
-  export default {
-    name: 'navMenu',
-    data () {
-      return {
-        activeIndex: '1'
-      }
-    },
-    props: {
-
-    },
-    methods: {
-    },
-    computed: {
-      isCollapsed() {
-        return window.innerWidth < 900;
-      },
-      isAdmin(){
-        return this.$store.state.isAdmin;
-      },
-      basket(){
-        return this.$store.state.basket;
-      },
-      basketSize(){
-        return this.basket.reduce((size, item) => size + (item.quantity), 0);
+import { store } from "../../store/store.js";
+export default {
+  name: "navMenu",
+  data() {
+    return {
+    };
+  },
+  props: {},
+  methods: {
+    handleSelect(e) {
+      switch (e) {
+        case "1":
+          this.$router.push("/");
+          break;
+        case "2":
+          this.$router.push("/products/");
+          break;
+        case "3":
+          this.$router.push("/orders/");
+          break;
+        case "4":
+          this.$router.push("/clients/");
+          break;
+        case "5":
+          this.$router.push("/basket/");
+          break;
       }
     }
+  },
+  computed: {
+    isCollapsed() {
+      return window.innerWidth < 900;
+    },
+    isAdmin() {
+      return this.$store.state.isAdmin;
+    },
+    basket() {
+      return this.$store.state.basket;
+    },
+    basketSize() {
+      return this.basket.reduce((size, item) => size + item.quantity, 0);
+    }
   }
+};
 </script>
 
