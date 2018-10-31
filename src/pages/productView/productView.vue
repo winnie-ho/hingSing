5e7db065167bdf046fba8b073d2832bd54c090aa
Winnie Ho <winnie-ho@hotmail.co.uk>
2018-10-31 10:20:05 +0000

    Adding a nav menu to the productView page.

--- src/pages/productView/productView.vue
+++ src/pages/productView/productView.vue
@@ -3,6 +3,7 @@
 
 <script>
 import siteHeader from "../../components/siteHeader/siteHeader.vue";
+import navMenu from "../../components/navMenu/navMenu.vue";
 import productForm from "../../components/productForm/productForm.vue";
 import breadcrumbBar from "../../components/breadcrumbBar/breadcrumbBar.vue";
 import { store } from '../../store/store.js'
@@ -11,6 +12,7 @@ export default {
   name: "productView",
   components: {
     "site-header": siteHeader,
+    "nav-menu": navMenu,
     "product-form": productForm,
     "breadcrumb-bar": breadcrumbBar
   },
